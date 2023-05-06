85ee89b34ba123a6a2237426509000f3551c1f41
bbonning4 <bbonning42@gmail.com>
2023-05-05 19:59:45 -0500

    part 1

---
+++ controllers/flights.js
@@ -0,0 +1,31 @@
+const Flight = require('../models/flight.js');
+
+module.exports = {
+    index,
+    new: newFlight,
+    create
+}
+
+async function index(req, res) {
+    res.render('flights/index', {
+        flights: await Flight.find({})
+    });
+}
+
+function newFlight(req, res) {
+    res.render('flights/new', { errorMsg: ''});
+}
+
+async function create(req, res) {
+    for(let key in req.body) {
+        if(req.body[key] === '') {
+            delete req.body[key];
+        }
+    }
+    try {
+        await Flight.create(req.body);
+        res.redirect('/flights');
+    } catch(err) {
+        res.render('flights/new', { errorMsg: err.message });
+    }
+}
--- models/flight.js
+++ models/flight.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 
 const flightSchema = new mongoose.Schema({
-    airline: {String,
+    airline: {type: String,
         enum: ['American', 'Southwest', 'United']
     },
-    airport: {String,
+    airport: {type: String,
         enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
         default: 'DEN'
     },
-    flightNo: {Number,
+    flightNo: {type: Number,
         min: 10,
         max: 9999,
         required: true
     },
-    departs: {Date,
+    departs: {type: Date,
         default: function() {
             return new Date(new Date().setFullYear(new Date().getFullYear() + 1))
-        }}
+        }
+    }
 })
+
+module.exports = mongoose.model('Flight', flightSchema);
